18116ceca9b1be5855e0589710bbd20f27000398
M A C W I N <mavzurovnurali@gmail.com>
2025-04-24 15:54:29 +0500

    4-misol yechimi

--- basic_function_parameter04.py
+++ basic_function_parameter04.py
@@ -1,4 +1,11 @@
 # Create a function named calculate_average that takes a list of numbers as a parameter.
+def calculate_average(numbers):
 # Inside the function, calculate the average of all the numbers in the given list.
+    calculate= sum(numbers) / len(numbers)
 # Return the average.
+    return calculate
+
 # Return the average.
+
+my_list=[1, 2, 3, 4]
+print(calculate_average(my_list))
