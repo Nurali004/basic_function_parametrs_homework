58550387df65284ba9eab6e3978ec9537145f922
M A C W I N <mavzurovnurali@gmail.com>
2025-04-24 15:16:35 +0500

    1-misol yechimi

--- basic_function_parameter01.py
+++ basic_function_parameter01.py
@@ -1,3 +1,6 @@
 # Create a function named get_data that takes no parameters.
+def get_data ():
 # Inside the function, define a list variable named data and assign it [1, 2, 3, 4, 5].
+data=[1,2,3,4]
 # Return the data list.
+return data
